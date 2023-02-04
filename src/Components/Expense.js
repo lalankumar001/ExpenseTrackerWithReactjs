3d7fff0416706a159d286ebc3fe685253bbfb2cd
lalankumar001 <tocsi.org@gmail.com>
2023-02-04 22:09:42 +0530

    filterAdded

--- src/Components/Expense.js
+++ src/Components/Expense.js
@@ -5,14 +5,19 @@ import ExpenseFilter from './ExpenseFilter'
 
 const Expense = (props) => {
   const[filterdYear, setFilteredYear] = useState('2020');
+  
   const filterChangeHandler=selectedYear=>{
     setFilteredYear(selectedYear);
   }
+  const filteredExpense = props.items.filter(expense =>{
+    return expense.date.getFullYear().toString()===filterdYear;
+  });
   return (
     <div className="expenses">
       <ExpenseFilter selected={filterdYear} onChangeFilter={filterChangeHandler} />
-    {props.items.map((expense)=>(
+    {filteredExpense.map((expense)=>(
       <ExpenseItem 
+      key ={expense.id}
       title={expense.title}
       amount ={expense.amount}
       date ={expense.date} 
--- src/Components/ExpenseDate.js
+++ src/Components/ExpenseDate.js
@@ -4,13 +4,14 @@ import './ExpenseDate.css'
 const ExpenseDate = (props) => {
   const month = props.date.toLocaleString('en-US', { month: 'long' });
   const day = props.date.toLocaleString('en-US', { day: '2-digit' });
+  const year = props.date.toLocaleString('en-US', { year: '2-digit' });
   // const year = new Date().getFullYear();
   
   return (
     <div className="expense-date">
-      <div className="expense-date__month">{month}</div>
-       {/* <div className="expense-date__year">{year}</div> */}
       <div className="expense-date__day">{day}</div> 
+      <div className="expense-date__month">{month}</div>
+       <div className="expense-date__year">{year}</div>
     </div>
   );
 }
